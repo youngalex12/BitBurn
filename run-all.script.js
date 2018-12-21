c0768a911db7974de60e6aa0ba2f0a06eb8277d3
youngalex12 <youngvideogamealex@gmail.com>
2018-12-21 15:56:06 -0800

    Update run-all.script.js

--- run-all.script.js
+++ run-all.script.js
@@ -1,7 +1,7 @@
-run('foodnstuff.script');
-run('nectar-net.script');
-run('sigma-cosmetics.script');
-run('neo-net.script');
-run('joesguns.script');
-run('hong-fang-tea.script');
-run('harakiri-sushi.script');
+run('foodnstuff.script.js');
+run('nectar-net.script.js');
+run('sigma-cosmetics.script.js');
+run('neo-net.script.js');
+run('joesguns.script.js');
+run('hong-fang-tea.script.js');
+run('harakiri-sushi.script.js');
