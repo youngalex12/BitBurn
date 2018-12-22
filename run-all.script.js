fb68d709fb8e38232d6eb2df52ef0ddec3d37671
youngalex12 <youngvideogamealex@gmail.com>
2018-12-21 16:03:50 -0800

    Update and rename run-all.script.js to run-all.script

--- run-all.script.js
+++
@@ -1,7 +0,0 @@
-run('foodnstuff.script.js');
-run('nectar-net.script.js');
-run('sigma-cosmetics.script.js');
-run('neo-net.script.js');
-run('joesguns.script.js');
-run('hong-fang-tea.script.js');
-run('harakiri-sushi.script.js');
